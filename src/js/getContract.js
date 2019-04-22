e3d24a7868cc8424138c29b8f96eaae8af03a3bd
shang <297157489@qq.com>
2019-04-22 14:30:08 +0800

    buil;d

--- src/js/getContract.js
+++ src/js/getContract.js
@@ -1,7 +1,8 @@
 import ethAbi from './eth_abi'
 import web3 from './web3'
 
-const address = '0xa73E7df87b0174f6d4a233be4f369267a57c17d0'
+// const address = '0xa73E7df87b0174f6d4a233be4f369267a57c17d0'
+const address = '0xCa1699f27E47CC577ed24a7CDC54676350e08510'
 let contract
 
 export default (account) => {
