168470d254268643fa4fe689355f5dfc1168c079
shang <297157489@qq.com>
2019-04-22 13:51:29 +0800

    dist path

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 const path = require('path')
 
 module.exports = {
-    publicPath: './',
+    publicPath: '/',
     configureWebpack: config => {
         if (process.env.NODE_ENV === 'development') {
             config.devtool = 'source-map'
