ff88e5c1f0790d47cdfd0f9a787b5143b7bf4694
shang <297157489@qq.com>
2019-05-15 14:48:49 +0800

    更换tron合约

--- src/js/address_config.js
+++ src/js/address_config.js
@@ -1,4 +1,4 @@
 export const eth = '0xCa1699f27E47CC577ed24a7CDC54676350e08510'
 export const ethSettle = '0x432203af55606d6e6B50d09cFCD18b918cBA3D0e'
-export const tron = 'TTrE9TMH5KzHZfrqT8rY5UBBCLkr6X8HeJ'
-export const tronSettle = 'TCbgmsxSxMv2a8svzRBkjJ7xcLsSrn5rjA'
+export const tron = 'TDJymTVRZ1aUes253E8t6a1QouEXMXLvDV'
+export const tronSettle = 'TTNeKhuWKAPd8xk6chcNYdks1FrSNtyEt7'
