889827bd63993cf790728d4a855f59cca16fa1b1
shang <297157489@qq.com>
2019-06-04 17:25:00 +0800

    build

--- src/js/eos/index.js
+++ src/js/eos/index.js
@@ -3,21 +3,21 @@ import ScatterJS, { Network } from "scatterjs-core";
 import ScatterEOS from "scatterjs-plugin-eosjs2";
 ScatterJS.plugins(new ScatterEOS());
 
-// const network = Network.fromJson({
-//     blockchain: "eos",
-//     host: "nodes.get-scatter.com",
-//     port: 443,
-//     protocol: "https",
-//     chainId: "aca376f206b8fc25a6ed44dbdc66547c36c6c33e3a119ffbeaef943642f0e906" // <-- this is the MAINNET
-// });
-
 const network = Network.fromJson({
     blockchain: "eos",
-    host: "jungle2.cryptolions.io",
-    port: 80,
-    protocol: "http",
-    chainId: "e70aaab8997e1dfce58fbfac80cbbb8fecec7b99cf982a9444273cbc64c41473" // <-- this is the tesetnet
+    host: "nodes.get-scatter.com",
+    port: 443,
+    protocol: "https",
+    chainId: "aca376f206b8fc25a6ed44dbdc66547c36c6c33e3a119ffbeaef943642f0e906" // <-- this is the MAINNET
 });
+
+// const network = Network.fromJson({
+//     blockchain: "eos",
+//     host: "jungle2.cryptolions.io",
+//     port: 80,
+//     protocol: "http",
+//     chainId: "e70aaab8997e1dfce58fbfac80cbbb8fecec7b99cf982a9444273cbc64c41473" // <-- this is the tesetnet
+// });
 
 const rpc = new Rpc.JsonRpc(network.fullhost());
 
